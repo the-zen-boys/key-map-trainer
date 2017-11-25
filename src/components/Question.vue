d7e108ca054fff253d381c06582089a2d9879b38
Daan van Hulst <daan.van.hulst@ascentio.nl>
2017-11-25 14:33:23 +0100

    Adds listener and dummy questions

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,23 +1,41 @@
 <template>
   <div>
+    <div>
+      <span>How does one {{currentQuestion.description}}?</span>
+    </div>
     <key-input-listener @key-combination-changed="_keyCombinationChanged"></key-input-listener>
   </div>
 </template>
 
 <script>
+import _ from 'lodash';
 import KeyInputListener from './KeyInputListener';
 
 export default {
-  name: 'HelloWorld',
+  name: 'Question',
+
+  created() {
+    // TODO fetch questions
+
+    // Randomly sort questions
+    if (this.questions && this.questions.length > 0) {
+      this.currentQuestion = this.questions[0];
+    }
+  },
+
   data() {
     return {
-      msg: 'Welcome to Your Vue.js App',
+      currentQuestion: {},
+      questions: [{ codes: [12, 13], description: 'copy selection' }],
     };
   },
 
   methods: {
     _keyCombinationChanged(combination) {
       console.log(combination);
+      if (_.isEmpty(_.xor(combination, this.currentQuestion.codes))) {
+        console.log('is equal');
+      }
     },
   },
   components: { KeyInputListener },
