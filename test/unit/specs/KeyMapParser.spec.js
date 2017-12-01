fed071f5f72a13b7c35bd82b379de39b1ee64c24
Mike de Klerk <mikedeklerk@gmail.com>
2017-12-01 17:51:11 +0100

    Adds unit test to check that 'description' is added to parsed key map object.

--- test/unit/specs/KeyMapParser.spec.js
+++ test/unit/specs/KeyMapParser.spec.js
@@ -24,7 +24,7 @@ describe('key-map-parser.js', () => {
     expect(actualCharCodes).to.deep.equal(expectedCharCodes);
   });
 
-  it('Can parse multiple shortcuts defined in the key map', () => {
+  it('can parse multiple shortcuts defined in the key map', () => {
     // Given
     const jsonString = `{
         "definitions": [
@@ -49,5 +49,32 @@ describe('key-map-parser.js', () => {
 
     // Then
     expect(actualCharCodes).to.deep.equal(expectedCharCodes);
+  });
+
+  it('adds description to parsed key map', () => {
+    // Given
+    const jsonString = `{
+        "definitions": [
+            {"[ctrl]": 17},
+            {"[alt]": 18},
+            {"[enter]": 13}
+        ],
+        "shortcuts": [{
+            "keys": ["[ctrl] k", "[enter]"],
+            "description": "Just some key short cut"
+        },{
+            "keys": ["[alt] l", "p l"],
+            "description": "Amazing lift off short cut"
+        }]
+    }`;
+    const keyMapParser = new KeyMapParser();
+    const expectedDescription = 'Amazing lift off short cut';
+
+    // When
+    const actual = keyMapParser.parseFromString(jsonString);
+    const actualCharCodes = actual[1].description;
+
+    // Then
+    expect(actualCharCodes).to.deep.equal(expectedDescription);
   });
 });
