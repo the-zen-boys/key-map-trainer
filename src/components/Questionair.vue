6c22144286982413d1ef55df4f93fb118bf156a8
MikeDeKlerk <mikedeklerk@gmail.com>
2017-12-10 13:08:23 +0100

    Adds method to check whether pressed key combination is correct.

--- src/components/Questionair.vue
+++ src/components/Questionair.vue
@@ -48,6 +48,26 @@ export default {
       if (_.isEmpty(_.xor(combination, this.currentQuestion.keys))) {
         console.log('is equal');
       }
+      console.log('Is correct: ');
+      const isCorrect = this._isCorrectKeyCombination(this.currentQuestion.keys, combination);
+      console.log(isCorrect);
+    },
+
+    _isCorrectKeyCombination: function (questionKeysCombi, pressedKeysCombi) {
+      if (questionKeysCombi.length !== pressedKeysCombi.length) return false;
+
+      for (let i = 0; i < questionKeysCombi.length; i++) {
+        var questionKeys = questionKeysCombi[i];
+        var pressedKeys = pressedKeysCombi[i];
+        if (questionKeys.length !== pressedKeys.length) return false;
+        for(let j = 0; j < questionKeys.length; j++) {
+          var questionKey = questionKeys[j];
+          var pressedKey = pressedKeys[j];
+          if (questionKey !== pressedKey) return false;
+        }
+      }
+      
+      return true;
     },
 
     showAnswer() {
